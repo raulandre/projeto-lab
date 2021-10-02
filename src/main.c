596e75faabede7b17b793c23c08b8a9ab7ae3a58
Raul André <raulandre.dev@gmail.com>
2021-10-01 21:34:51 -0300

    Limpando o projeto

--- src/main.c
+++ src/main.c
@@ -2,14 +2,6 @@
 
 int main(void)
 {
-	printf("Ola mundo!\n");
-<<<<<<< HEAD
-	while(1==1){
-	printf("\nRAUL CORNO\n");
-	}
-=======
-	printf("\nTestando o terminal ubuntu\n");
-	printf("Seguinte meu PARCEIRO...\n");
->>>>>>> 7f119d727fe54a2216be225e670fd82974f92f8a
+	
 	return 0;
 }

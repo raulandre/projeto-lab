5a5e95af3928eda935e15e1db3df82f1f208cbd7
gabrielpinacio <gabrielpinacio@protonmail.com>
2021-10-24 21:23:29 -0300

    Criação de novas telas

--- include/mainmenu.h
+++ include/mainmenu.h
@@ -1,6 +1,12 @@
 #ifndef MENUPRINCIPAL_H
 #define MENUPRINCIPAL_H
+#include <ncurses.h>
 
+void destroy_win(WINDOW *);
+WINDOW *crete_newwin(int, int, int, int);
+void rowMenu(int, int);
+void clientsMenu(int, int);
+void mainMenu();
 void printMenu();
 
 #endif 
--- src/mainmenu.c
+++ src/mainmenu.c
@@ -1,24 +1,14 @@
 #include <ncurses.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "mainmenu.h"
 
 
 void destroy_win(WINDOW *local_win){	
       wclear(local_win);
-    /* Os parâmetros usados são
-    * 1. win: a janela na qual operar
-    * 2. ls: caractere a ser usado para o lado esquerdo da janela
-    * 3. rs: caractere a ser usado para o lado direito da janela
-    * 4. ts: caractere a ser usado na parte superior da janela
-    * 5. bs: caractere a ser usado na parte inferior da janela
-    * 6. tl: caractere a ser usado para o canto superior esquerdo da janela
-    * 7. tr: caractere a ser usado no canto superior direito da janela
-    * 8. bl: caractere a ser usado no canto inferior esquerdo da janela
-    * 9. br: caractere a ser usado no canto inferior direito da janela
-    */ 
-	wborder (local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-	wrefresh(local_win);
-	delwin(local_win);
+      wborder (local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+      wrefresh(local_win);
+      delwin(local_win);
 }
 
 WINDOW *create_newwin(int height, int width, int starty, int startx){
@@ -31,16 +21,118 @@ WINDOW *create_newwin(int height, int width, int starty, int startx){
   return local_win;
 }
 
-void printMenu(){
-	// inicializando ncurses
-	initscr();
-	noecho();
-	cbreak();
+void rowMenu(int yMax, int xMax){
+	WINDOW * rowwin;
 
+	rowwin = create_newwin(8,40, (yMax/2)-4, (xMax/2)-20);
+	refresh();
+	box(rowwin,0,0);
+	wrefresh(rowwin);
+
+	keypad(rowwin, true);
+	char rowChoices [4] [40] = {"Mostrar a Fila", "Adicionar","Chamar o Proximo", "Voltar"};
+	int rowChoice, highlight = 0, rowOption;
+
+	
+	while(1) {	
+		for(int i = 0; i < 4; i++){
+			if(i == highlight)
+				wattron(rowwin,A_REVERSE);
+			mvwprintw(rowwin, i+2, 1,"\t%s", rowChoices[i]);
+			wattroff(rowwin,A_REVERSE);
+			wrefresh(rowwin);
+		}
+		rowChoice = wgetch(rowwin);
+
+		switch(rowChoice){
+			case KEY_UP:
+				highlight--;	
+				if(highlight == -1)
+					highlight = 2;
+				break;
+			case KEY_DOWN:
+				highlight++;
+				if(highlight == 4)
+					highlight = 0;
+				break;
+			case 10:
+				rowOption = highlight+1;
+				switch(rowOption){
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3: 
+						break;
+					case 4:
+						mainMenu();
+						break;
+
+				}
+		}
+	}
+
+}
+
+void clientsMenu(int yMax, int xMax){
+	WINDOW * clientwin;
+
+	clientwin = create_newwin(8,40, (yMax/2)-4, (xMax/2)-20);
+	refresh();
+	box(clientwin,0,0);
+	wrefresh(clientwin);
+
+	keypad(clientwin, true);
+	//esolha
+	char clientsChoices [3] [22] = {"Criar um Cliente", "Listar Um Cliente", "Voltar"};
+	int clientChoice, highlight = 0, clientOption;
+                                                                                            
+	
+	while(1) {	
+		for(int i = 0; i < 3; i++){
+			if(i == highlight)
+				wattron(clientwin,A_REVERSE);
+			mvwprintw(clientwin, i+2, 1,"\t%s", clientsChoices[i]);
+			wattroff(clientwin,A_REVERSE);
+			wrefresh(clientwin);
+		}
+		clientChoice = wgetch(clientwin);
+                                                                                            
+		switch(clientChoice){
+			case KEY_UP:
+				highlight--;	
+				if(highlight == -1)
+					highlight = 2;
+				break;
+			case KEY_DOWN:
+				highlight++;
+				if(highlight == 3)
+					highlight = 0;
+				break;
+			case 10:
+				clientOption = highlight+1;
+				switch(clientOption){
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						mainMenu();
+						break;
+                                                                                            
+				}
+		}
+	}
+
+
+}
+
+void mainMenu(){
+	
 	// tamanho da tela
 	int yMax, xMax;
 	getmaxyx(stdscr, yMax, xMax);
-
+	
 	//criando box
 	WINDOW * win;
 	
@@ -50,13 +142,14 @@ void printMenu(){
 	box(win,0,0);
 	wrefresh(win);
 
-	//intecao com o teclado
+	//interacao com o teclado
 	keypad(win, true);
 
 	//menu
 	char choices[3] [9] ={"Clientes", "Fila","Sair"};
-	int choice, highlight = 0;
+	int choice, highlight = 0, option;
 
+	
 	while(1) {	
 		for(int i = 0; i < 3; i++){
 			if(i == highlight)
@@ -78,20 +171,34 @@ void printMenu(){
 				if(highlight == 3)
 					highlight = 0;
 				break;
-			default:
-				break;
-		}
-		if(choice ==10) {
-			destroy_win(win);
-			win = create_newwin(12, 10, (yMax/2)-5,(xMax/2)-5);
-			break;
+			case 10:
+				option= highlight +1;
+				switch(option){
+					case 1:
+						clientsMenu(yMax, xMax);
+						break;
+					case 2:
+						rowMenu(yMax, xMax);
+						break;
+					case 3:
+						refresh();
+						endwin();
+						exit(0);
+						break;
+
+				}
 		}
 	}
+}
 
 
-	//programa me espere, por favor
-	getch();
-	endwin();
 
+void printMenu(){
+	// inicializando ncurses
+	initscr();
+	noecho();
+	cbreak();
+	
+	mainMenu();
 
 }
